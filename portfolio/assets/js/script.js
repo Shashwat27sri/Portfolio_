5acef4a4295c6e6cfac8acf86eafd03c8e7e9b9a
Shashwat27sri <shashwatsu27@gmail.com>
2023-08-09 21:47:30 +0530

    started about section

--- portfolio/assets/js/script.js
+++ portfolio/assets/js/script.js
@@ -30,28 +30,28 @@ $(document).ready(function () {
   });
 
   // smooth scrolling
-  $('a[href*="#"]').on('click', function (e) {
-      e.preventDefault();
-      $('html, body').animate({
-          scrollTop: $($(this).attr('href')).offset().top,
-      }, 500, 'linear')
-  });
+//   $('a[href*="#"]').on('click', function (e) {
+//       e.preventDefault();
+//       $('html, body').animate({
+//           scrollTop: $($(this).attr('href')).offset().top,
+//       }, 500, 'linear')
+//   });
 
   // <!-- emailjs to mail contact form data -->
-  $("#contact-form").submit(function (event) {
-      emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+//   $("#contact-form").submit(function (event) {
+//       emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
 
-      emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
-          .then(function (response) {
-              console.log('SUCCESS!', response.status, response.text);
-              document.getElementById("contact-form").reset();
-              alert("Form Submitted Successfully");
-          }, function (error) {
-              console.log('FAILED...', error);
-              alert("Form Submission Failed! Try Again");
-          });
-      event.preventDefault();
-  });
+//       emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
+//           .then(function (response) {
+//               console.log('SUCCESS!', response.status, response.text);
+//               document.getElementById("contact-form").reset();
+//               alert("Form Submitted Successfully");
+//           }, function (error) {
+//               console.log('FAILED...', error);
+//               alert("Form Submission Failed! Try Again");
+//           });
+//       event.preventDefault();
+//   });
   // <!-- emailjs to mail contact form data -->
 
 });
