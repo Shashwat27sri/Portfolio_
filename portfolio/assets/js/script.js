d6797f88518a3ab8d4bf0c5c2720fca2a96f7ce1
Shashwat27sri <shashwatsu27@gmail.com>
2023-09-07 20:53:50 +0530

    Added updated resume, figured hamburger , changed avatar

--- portfolio/assets/js/script.js
+++ portfolio/assets/js/script.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
 
   $('#menu').click(function () {
       $(this).toggleClass('fa-times');
-      $('.navbar').toggleClass('nav-toggle');
+      $('.tags').toggleClass('nav-toggle');
   });
 
   $(window).on('scroll load', function () {
       $('#menu').removeClass('fa-times');
-      $('.navbar').removeClass('nav-toggle');
+      $('.tags').removeClass('nav-toggle');
 
       if (window.scrollY > 60) {
           document.querySelector('#scroll-top').classList.add('active');
@@ -16,26 +16,26 @@ $(document).ready(function () {
       }
 
       // scroll spy
-      $('section').each(function () {
-          let height = $(this).height();
-          let offset = $(this).offset().top - 200;
-          let top = $(window).scrollTop();
-          let id = $(this).attr('id');
+      // $('section').each(function () {
+      //     let height = $(this).height();
+      //     let offset = $(this).offset().top - 200;
+      //     let top = $(window).scrollTop();
+      //     let id = $(this).attr('id');
 
-          if (top > offset && top < offset + height) {
-              $('.navbar ul li a').removeClass('active');
-              $('.navbar').find(`[href="#${id}"]`).addClass('active');
-          }
-      });
+      //     if (top > offset && top < offset + height) {
+      //         $('.tags ul li a').removeClass('active');
+      //         $('.tags').find(`[href="#${id}"]`).addClass('active');
+      //     }
+      // });
   });
 
   // smooth scrolling
-//   $('a[href*="#"]').on('click', function (e) {
-//       e.preventDefault();
-//       $('html, body').animate({
-//           scrollTop: $($(this).attr('href')).offset().top,
-//       }, 500, 'linear')
-//   });
+  // $('a[href*="#"]').on('click', function (e) {
+  //     e.preventDefault();
+  //     $('html, body').animate({
+  //         scrollTop: $($(this).attr('href')).offset().top,
+  //     }, 500, 'linear')
+  // });
 
   // <!-- emailjs to mail contact form data -->
 //   $("#contact-form").submit(function (event) {
